971d68aaaf1386146f375190c9b7c5611c1f67e9
vishangi <vishangid@gmail.com>
2022-06-06 23:12:14 +0530

    Added : Gambler bet everyday

---
+++ GamblerGame.java
@@ -0,0 +1,12 @@
+package com.game;
+import java.util.Random;
+public class GamblerGame {
+	// UC1 : Initialized the stack every day & bet every day
+	public static int STACK_EVERY_DAY = 100;
+	public static final int BET_EVERY_DAY = 1;
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the Gambling game!");
+	}
+}
